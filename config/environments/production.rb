f97dabadd11a8e49cf28d65eba486f2efff5e2a3
Matthew M. Keeler <keelerm84@gmail.com>
2013-10-27 00:35:28 -0400

    Turn off debugging of assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ TortugasLlcCom::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.assets.debug = false
+
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
