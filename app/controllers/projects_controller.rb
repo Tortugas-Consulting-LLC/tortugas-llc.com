8393f9f7cd08a1c0519fad29a3f7f94a67f50e7f
Matthew M. Keeler <keelerm84@gmail.com>
2013-11-01 19:25:53 -0400

    Order projects by title

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,7 +7,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = current_user.projects
+    @projects = current_user.projects.order("title")
   end
 
   # GET /projects/1
